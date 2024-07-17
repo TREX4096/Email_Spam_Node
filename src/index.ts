ed018106b16548f9c370377653c1f90d20a7374d
Prasoon <prasoontrex4096@gmail.com>
2024-07-17 14:07:02 +0530

    Rendevzous24

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,9 @@ var transporter = nodemailer.createTransport({
 });
 
 // Read the image file
-var img = fs.readFileSync("/path/file").toString("base64");
+var img = fs
+  .readFileSync("/home/trex4096/Desktop/WEb3/Projects/EmailSender/public/1.jpg")
+  .toString("base64");
 
 // Function to read email addresses from Excel file
 function readEmailAddresses(filePath: any) {
@@ -22,7 +24,7 @@ function readEmailAddresses(filePath: any) {
   const sheetName = workbook.SheetNames[0];
   const worksheet = workbook.Sheets[sheetName];
   const data = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
-
+  console.log(data);
   return { data, workbook, worksheet };
 }
 
@@ -41,9 +43,8 @@ const blueStyle = {
 
 // Function to change row color
 function changeRowColor(worksheet: any, rowIndex: number) {
-  for (let col = 0; col < 2; col++) {
+  for (let col = 0; col < 4; col++) {
     const cellRef = xlsx.utils.encode_cell({ r: rowIndex - 1, c: col });
-    console.log(worksheet[cellRef]);
     if (worksheet[cellRef]) {
       worksheet[cellRef].s = blueStyle;
     }
@@ -51,10 +52,14 @@ function changeRowColor(worksheet: any, rowIndex: number) {
 }
 
 // Email addresses from Excel file
-const { data, workbook, worksheet } = readEmailAddresses("/path/file");
+const { data, workbook, worksheet } = readEmailAddresses(
+  "/home/trex4096/Desktop/WEb3/Projects/EmailSender/public/Sample.xlsx",
+);
 
 const emailAddresses: string[] = [];
 const userName: string[] = [];
+const company_Name: string[] = [];
+const company_Size: string[] = [];
 const length = data.length;
 
 for (let i = 1; i < length; i++) {
@@ -68,22 +73,67 @@ for (let i = 1; i < length; i++) {
   if (typeof username === "string") {
     userName.push(username);
   }
+
+  //@ts-ignore
+  const company = data[i][2];
+  if (typeof company === "string") {
+    company_Name.push(company);
+  }
+
+  //@ts-ignore
+  const size = data[i][3];
+  if (typeof size === "string") {
+    company_Size.push(size);
+  }
 }
 
 console.log(emailAddresses);
 console.log(userName);
+console.log(company_Name);
+console.log(company_Size);
+
+function scripts(user: string, type: string) {
+  if (type == "Big") {
+    return `Hey ${user},<br/><br/>
+    We know that your company has an esteemed reputation in the market. However, we understand that branding is a continuous process, and to stand out among other companies, you are always looking for unique ways to brand and provide a personalized experience to your target audience. We can help you achieve this.<br/><br/>
+    We are students of IIT Delhi and are excited to introduce you to the 49th edition of Rendezvous, Asia's most significant cultural festival, organized annually at IIT Delhi. Every year, Rendezvous draws over 160,000+ impressions from 1600+ institutions. In 4 days, we conducted 300+ events & pronates, which previously have witnessed 50+ artists like Sonu Nigam, Guru Randhawa, Salim Suleman and comedians such as Zakir Khan and Anubhav Bassi.<br/><br/>
+    We partner with companies like yours each year to help them connect with their target audience and create a personalized brand experience. Collaborations have been highly successful, with companies seeing significant engagement and brand recognition at our events.<br/><br/>
+    Here's how we helped companies like yours in the past:<br/>
+    • Engage a large audience over four days.<br/>
+    • Stage Time during the Events & premium artist Pronites.<br/>
+    • Customise brand experiences with Tailored Events & Dedicated Zones.<br/>
+    • Gorilla online & Offline Marketing strategy.<br/><br/>
+    I'm eager to hear your thoughts on this exciting opportunity. If you're interested in reaching potential customers in a unique and engaging way, I'd love to discuss this further with you over a call.<br/><br/>
+    Regards,<br/>
+    `;
+  } else {
+    return `Hey ${user},<br/><br/>
+    I stumbled upon your company recently and was genuinely impressed with the offerings. There's significant untapped potential, especially in gaining popularity among younger audiences.<br/><br/>
+    I have an excellent way for you guys to push your company to reach and engage with its target audience. I'm excited to introduce you to the 49th edition of Rendezvous, Asia's most significant cultural festival, organized annually at IIT Delhi. Every year, Rendezvous draws over 160,000+ impressions from 1600+ institutions. In 4 days, we conducted 300+ events & pronate, which previously witnessed 50+ artists like Sonu Nigam, Guru Randhawa, Salim Suleman and comedians such as Zakir Khan and Anubhav Bassi.<br/><br/>
+    We partner with companies like yours each year to help them connect with their target audience and create a personalized brand experience. Our past collaborations have been highly successful, with companies seeing significant engagement and brand recognition at our events.<br/><br/>
+    Here's how we helped companies like yours in the past:<br/>
+    • Engage a large audience over four days.<br/>
+    • Stage Time during the Events & premium artist Pronites.<br/>
+    • Customise brand experiences with Tailored Events & Dedicated Zones.<br/>
+    • Online & Offline Promotion.<br/><br/>
+    I'm eager to hear your thoughts on this exciting opportunity. If you're interested in reaching potential customers in a unique and engaging way, I'd love to discuss this further with you over a call.<br/><br/>
+    Regards,<br/>
+    `;
+  }
+}
 
 const noOfRecipients = emailAddresses.length;
 
 for (let i = 0; i < noOfRecipients; i++) {
+  const mail_Script = scripts(userName[i], company_Size[i]);
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: emailAddresses[i],
-    subject: `Sending Email using Node.js to ${userName[i]}`,
-    html: `
-      <p>That was easy!</p>
-      <p>Your Designation: <strong>Your Designation Here</strong></p>
-      <img src="cid:unique@nodemailer.com" style="width: 200px; height: auto;"/>
+    subject: `Association of ${company_Name[i]} x IIT Delhi`,
+    html:
+      mail_Script +
+      `
+      <img src="cid:unique@nodemailer.com" style="width: 20%; height: auto;"/>
     `,
     attachments: [
       {
